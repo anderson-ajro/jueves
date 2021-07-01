2e5501a82387a0c9c258fcd310078873431fd8d2
Anderson <83408814+anderson-ajro@users.noreply.github.com>
2021-07-01 19:53:31 -0300

    Update register-sw.js

--- js/register-sw.js
+++ js/register-sw.js
@@ -10,16 +10,54 @@ if ('serviceWorker' in navigator) {
   }
   
   // Event Listener para Offline/ Online Status
-  
-  // A veces este evento falla, ojo!
-  // Sirve para saber si el navegador esta offline, cuando entramos offline. 
-  // Es decir, no se disparo los eventos de arriba aun, y necesito conocer el estado.
-  // 
-  
- window.addEventListener('offline', event => {
+  window.addEventListener('offline', event => {
     document.querySelector('body').classList.add('offline');
   });
   
   window.addEventListener('online', event => {
     document.querySelector('body').classList.remove('offline');
   });
+  
+  // A veces este evento falla, ojo!
+  // Sirve para saber si el navegador esta offline, cuando entramos offline. 
+  // Es decir, no se disparo los eventos de arriba aun, y necesito conocer el estado.
+  // 
+  
+  if (!navigator.onLine) {
+    document.querySelector('body').classList.add('offline');
+  }
+
+
+  
+const isOnline = async () => {
+  try {
+    
+    const response = await fetch('https://code.jquery.com/jquery-3.6.0.slim.min.js', {cache: "no-store"});
+    
+    if (response.url == "https://jueves-gamma.vercel.app/offline.html"){
+      return false;
+    }
+    
+    return true;
+  } catch (error) {
+    console.error('offline and rejected', error)
+    return false;
+  }
+}
+
+isOnline().then(
+  // Resolve, estamos online
+  resp => {
+    if (resp){
+      // Creo un evento
+      var evento = new CustomEvent("onInitOnline", {});
+
+      // Lo Disparo!
+      document.dispatchEvent(evento);
+    } else {
+      $main.classList.add('offline');
+    }
+  },
+  // Reject, estamos offline
+  ()=> {$main.classList.add('offline');}
+);
